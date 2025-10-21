89c64791467a8ded240804e2d5996f0f75855b6d
Xentauros <vicentepeinadoalcedo@gmail.com>
2025-10-21 13:05:47 +0200

    4

---
+++ T3/Ejercicios/JavaScript/eje4.js
@@ -0,0 +1,4 @@
+let original = "JavaScript es un lenguaje de programación muy popular.";
+let extraida = original.substring(6, 20);
+original = document.getElementById("original").textContent;
+extraida = document.getElementById("extraida").textContent;
